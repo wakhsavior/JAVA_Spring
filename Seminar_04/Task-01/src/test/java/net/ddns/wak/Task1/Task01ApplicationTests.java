cccded1bb79e4818f122cc85d32c23c0a9a93590
wakhsavior <wakhsavior@gmail.com>
2024-02-06 00:33:22 +0300

    Add Seminar_04

---
+++ Seminar_04/Task-01/src/test/java/net/ddns/wak/Task1/Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.wak.Task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Seminar_04/Task-03/src/test/java/net/ddns/wak/Task3/Task03ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.wak.Task3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

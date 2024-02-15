a37ee6a832e7ee02aea178c729fd79f2ccba5a87
wakhsavior <wakhsavior@gmail.com>
2024-02-15 23:49:35 +0300

    Add Seminar_07

--- MVC/src/main/java/my/spring/MVC/SMS/controllers/StudentController.java
+++
@@ -1,7 +0,0 @@
-package my.spring.MVC.SMS.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class StudentController {
-}
--- MVC/src/main/java/my/spring/MVC/SMS/models/Student.java
+++
@@ -1,5 +0,0 @@
-package my.spring.MVC.SMS.models;
-
-public class Student {
-    private final String
-}

29d35da4ce4aede9d6800244454c2dba9f45300a
wakhsavior <wakhsavior@gmail.com>
2024-03-06 23:20:31 +0300

    Add Lecture 09

---
+++ Lecture_09/Zuul/src/main/java/ru/gb/Zuul/ZuulApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.Zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
